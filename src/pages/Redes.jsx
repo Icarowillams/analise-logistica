5e26cf86ca67ac45f4f1d70453f0e9d63d57e27e
Paulo Rodrigo <paulordkt@gmail.com>
2025-11-29 13:03:37 +0000

    File changes

--- src/pages/Redes.jsx
+++ src/pages/Redes.jsx
@@ -9,6 +9,7 @@ import BulkImportModal from '@/components/forms/BulkImportModal';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Badge } from '@/components/ui/badge';
 
