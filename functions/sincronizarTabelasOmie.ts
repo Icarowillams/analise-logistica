cc09fdb0fd3dc53446fb68fcc3ec0da28f03ff51
Paulo Rodrigo <paulordkt@gmail.com>
2026-02-09 21:38:20 +0000

    File changes

--- functions/sincronizarTabelasOmie.ts
+++ functions/sincronizarTabelasOmie.ts
@@ -210,11 +210,11 @@ Deno.serve(async (req) => {
         cCodIntTabPreco: codInt,
         cNome: tabela.nome,
         cCodigo: tabela.nome.substring(0, 20).toUpperCase().replace(/\s+/g, '_'),
-        cOrigem: "TBL",
-        produtos: { cTodosProdutos: "S", nCodFamilia: 0, cNCM: "", nCodCaract: 0, cConteudo: "", nCodFornec: 0 },
+        cOrigem: "PRD",
+        produtos: { cTodosProdutos: "S" },
         clientes: { cTodosClientes: "S" },
-        outrasInfo: { nCodOrigTab: 0, nPercAcrescimo: 0, nPercDesconto: 0 },
-        caracteristicas: { cTemValidade: "N", dDtInicial: "", dDtFinal: "", cTemDesconto: "N", nDescSugerido: 0, nPercDescMax: 0, cArredPreco: "N" }
+        outrasInfo: { nPercAcrescimo: 0, nPercDesconto: 0 },
+        caracteristicas: { cTemValidade: "N", cTemDesconto: "N", cArredPreco: "N" }
       };
 
       let result;
