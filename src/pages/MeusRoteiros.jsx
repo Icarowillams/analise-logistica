20c735cd1f881a819b6462d32487724e0c06de9d
Paulo Rodrigo <paulordkt@gmail.com>
2026-02-20 20:32:36 +0000

    File changes

--- src/pages/MeusRoteiros.jsx
+++ src/pages/MeusRoteiros.jsx
@@ -775,6 +775,9 @@ function CheckinButton({ cliente, roteiroId, vendedor, onSuccess, reagendamentoI
 
     const agora = new Date();
     const numeroVisita = `V${agora.getTime()}-${vendedor.id.substring(0, 8)}`;
+    const dataHoje = getLocalDateStr(agora);
+    const horaAgora = getLocalTimeStr(agora);
+    const checkinISO = getLocalISOString(agora);
 
     const motivoNaoAtendObj = motivosNaoAtend.find(m => m.id === motivoNaoAtendimento);
 
@@ -787,8 +790,8 @@ function CheckinButton({ cliente, roteiroId, vendedor, onSuccess, reagendamentoI
       cliente_nome: cliente.cliente_nome,
       cliente_codigo: cliente.cliente_codigo,
       cliente_cidade: cliente.cliente_cidade,
-      data_visita: agora.toISOString().split('T')[0],
-      checkin_time: agora.toISOString(),
+      data_visita: dataHoje,
+      checkin_time: checkinISO,
       checkin_latitude: locationData.latitude,
       checkin_longitude: locationData.longitude,
       status: 'nao_atendido',
@@ -803,8 +806,8 @@ function CheckinButton({ cliente, roteiroId, vendedor, onSuccess, reagendamentoI
       cliente_nome: cliente.cliente_nome,
       vendedor_id: vendedor.id,
       vendedor_nome: vendedor.nome,
-      data_visita: agora.toISOString().split('T')[0],
-      hora_checkin: agora.toTimeString().split(' ')[0],
+      data_visita: dataHoje,
+      hora_checkin: horaAgora,
       latitude_checkin: locationData.latitude,
       longitude_checkin: locationData.longitude,
       pedido_solicitado: null
