c8114a9c3616bf23bb92b96272a26efda0e6720f
Paulo Rodrigo <paulordkt@gmail.com>
2026-03-13 12:51:02 +0000

    File changes

--- src/pages/Funcionarios.jsx
+++ src/pages/Funcionarios.jsx
@@ -100,6 +100,7 @@ export default function Funcionarios() {
       funcao: '', 
       departamento_id: '',
       supervisor_id: '',
+      supervisor_ids: [],
       telefone: '',
       latitude: '',
       longitude: '',
